7198174055935e698d8d1a910272d34c1c501741
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-09-10 09:07:21 -0400

    Refatora integerToRomain para testConvertFourToRomainIV passar

--- src/main/java/br/com/alexandremacedo/katas/RomanNumerals.java
+++ src/main/java/br/com/alexandremacedo/katas/RomanNumerals.java
@@ -5,6 +5,8 @@ public class RomanNumerals {
     public String integerToRoman(int number) {
         if(number == 1){
             return "I";
+        }else if (number == 4){
+            return "IV";
         }
         return Integer.toString(number);
     }
