88f5b683e5dc8b636f31970f07888e0abc251aa2
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-09-09 22:19:07 -0400

    Implementa testReturnNumber

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+import br.com.alexandremacedo.FizzBuzz;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FizzBuzzTest {
+
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    void testReturnNumber() {
+        assertEquals("1", fizzBuzz.fizzBuzz(1));
+    }
+}
