64ff634f3e71b07e2fb789b5542dbf09f29bf1a6
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-09-10 09:01:46 -0400

    Cria Kata para test Roman Numerals e implementa testConvertOneToRomainI

---
+++ src/test/java/RomanNumeralsTest.java
@@ -0,0 +1,13 @@
+import br.com.alexandremacedo.katas.RomanNumerals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RomanNumeralsTest {
+
+    RomanNumerals romanNumerals = new RomanNumerals();
+
+    @Test
+    void testConvertOneToRomainI(){
+        assertEquals("I",romanNumerals.integerToRoman(1));
+    }
+}
