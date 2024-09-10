306e51645d28ee0c72def2bc2f3f8e376220a3e4
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-09-09 23:42:20 -0400

    Refatora metodo fizzBuzz para testNegativeNumber passar

--- src/main/java/br/com/alexandremacedo/katas/FizzBuzz.java
+++ src/main/java/br/com/alexandremacedo/katas/FizzBuzz.java
@@ -2,12 +2,15 @@ package br.com.alexandremacedo.katas;
 
 public class FizzBuzz {
     public String fizzBuzz(int number) {
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
-        } else if (number % 3 == 0) {
-            return "Fizz";
-        } else if (number % 5 == 0) {
-            return "Buzz";
+        if (number < 0) {
+            if (number % 3 == 0 && number % 5 == 0) {
+                return "FizzBuzz";
+            } else if (number % 3 == 0) {
+                return "Fizz";
+            } else if (number % 5 == 0) {
+                return "Buzz";
+            }
+            return "Number negative";
         }
         return Integer.toString(number);
     }
