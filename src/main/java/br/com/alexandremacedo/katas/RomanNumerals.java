a85b891aae1cf8264bc142ff1964076ee6087e77
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-09-11 22:47:25 -0400

    Refatora método integerRoman para utilizar Map com LinkedHashMap

--- src/main/java/br/com/alexandremacedo/katas/RomanNumerals.java
+++ src/main/java/br/com/alexandremacedo/katas/RomanNumerals.java
@@ -1,15 +1,30 @@
 package br.com.alexandremacedo.katas;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RomanNumerals {
 
+
+    private static final Map<Integer, String> romanMap = new LinkedHashMap<>();
+
+    static {
+        romanMap.put(9,"IX");
+        romanMap.put(5, "V");
+        romanMap.put(4, "IV");
+        romanMap.put(1, "I");
+    }
+
     public String integerToRoman(int number) {
-        if(number == 1){
-            return "I";
-        }else if (number == 4){
-            return "IV";
-        }else if(number == 9){
-            return "IX";
+
+        StringBuilder result = new StringBuilder();
+
+        for (Map.Entry<Integer, String> entry : romanMap.entrySet()) {
+            while (number >= entry.getKey()) {
+                result.append(entry.getValue());
+                number -= entry.getKey();
+            }
         }
-        return Integer.toString(number);
+        return result.toString();
     }
 }
